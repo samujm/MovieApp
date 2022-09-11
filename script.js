79795f7031a15e0c53bd465a0099d8375ea7e3d5
Samantha González <samujm76@gmail.com>
2022-09-10 22:39:43 -0500

    Cambio del DOM y correcciones del css

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=0ccaaac6871871759482344c8579e64b&query="'
 
+const main = document.getElementById('main')
 const form = document.getElementById('form')
-
 const search = document.getElementById('search')
 
 //Get initial movies
@@ -14,7 +14,43 @@ async function getMovies(url){
     const data = await res.json()
 
     console.log(data.results)
+    showMovies(data.results)
 }
+
+function showMovies(movies){
+    main.innerHTML = ''
+
+    movies.forEach((movie) => {
+        const { title, poster_path, vote_average, overview } = movie
+
+        const movieEl = document.createElement('div')
+        movieEl.classList.add('movie')
+
+        movieEl.innerHTML = `
+            <img src="${IMG_PATH + poster_path}" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview</h3>
+                ${overview}
+            </div>
+        `
+        main.appendChild(movieEl)
+    });
+}
+
+function getClassByRate(vote){
+    if (vote >= 8) {
+        return 'green'
+    } else if (vote >= 5) {
+        return 'orange'
+    } else {
+        return 'red'
+    }
+}
+
 
 form.addEventListener('submit', (e)=>{
     e.preventDefault()
